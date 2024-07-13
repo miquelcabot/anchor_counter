8ad3037a02d237fe5285f5a9e4565bc800688550
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-13 08:56:54 +0200

    add tests

--- tests/anchor_counter.ts
+++ tests/anchor_counter.ts
@@ -1,16 +1,48 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { AnchorCounter } from "../target/types/anchor_counter";
+import { Counter } from "../target/types/counter";
+import { Keypair } from "@solana/web3.js";
 
-describe("anchor_counter", () => {
+describe("counter", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.AnchorCounter as Program<AnchorCounter>;
+  const program = anchor.workspace.Counter as Program<Counter>;
+
+  const counterAccount = new Keypair();
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    // Invoke the initialize instruction
+    const transactionSignature = await program.methods
+        .initialize()
+        .accounts({
+            counter: counterAccount.publicKey,
+        })
+        .signers([counterAccount]) // include counter keypair as additional signer
+        .rpc({ skipPreflight: true });
+
+    // Fetch the counter account data
+    const accountData = await program.account.counter.fetch(
+        counterAccount.publicKey
+    );
+
+    console.log(`Transaction Signature: ${transactionSignature}`);
+    console.log(`Count: ${accountData.count}`);
+  });
+
+  it("Increment", async () => {
+    const transactionSignature = await program.methods
+        .increment()
+        .accounts({
+            counter: counterAccount.publicKey,
+        })
+        .rpc();
+
+    const accountData = await program.account.counter.fetch(
+        counterAccount.publicKey
+    );
+
+    console.log(`Transaction Signature: ${transactionSignature}`);
+    console.log(`Count: ${accountData.count}`);
   });
 });
